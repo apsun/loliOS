d6f19376e9c59ce6a0a4d2dd1f0b1884fe833623
Emre Ulusoy <ulusoy2@illinois.edu>
2016-11-26 10:16:14 +0000

    I'll be working on this for extra cred
    git-svn-id: https://subversion.ews.illinois.edu/svn/fa16-ece391/_projects/loliOS@12151 0df279af-beec-47f8-8c4d-85a890efaaa8

---
+++ student-distrib/kmalloc_and_kfree.c
@@ -0,0 +1,4 @@
+/* If anybody works on this without my permission I'm gonna 
+ * shoot Hertin in the kneecaps I swear to god.
+ *
+ */
---
+++ student-distrib/kmalloc_and_kfree.h
@@ -0,0 +1,21 @@
+
+
+/* this is useless
+typedef struct memory_map
+{
+    uint32_t size;
+    uint32_t base_addr_low;
+    uint32_t base_addr_high;
+    uint32_t length_low;
+    uint32_t length_high;
+    uint32_t type;
+} memory_map_t;
+*/
+
+typedef struct memory_map_entry
+{
+    uint32_t id;
+    uint32_t size;
+    uint32_t next;
+    uint32_t prev;
+} mm_block_t;
