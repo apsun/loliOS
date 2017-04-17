ac0f11a2c7d810f0e6de8c55433947d5f23f54c4
Andrew Sun <apsun2@illinois.edu>
2017-04-17 16:59:08 +0000

    Add echo
    git-svn-id: https://subversion.ews.illinois.edu/svn/fa16-ece391/_projects/loliOS@14045 0df279af-beec-47f8-8c4d-85a890efaaa8

---
+++ syscalls/ece391echo.c
@@ -0,0 +1,17 @@
+#include <stdint.h>
+
+#include "ece391support.h"
+#include "ece391syscall.h"
+
+int main(void)
+{
+    uint8_t args[4096];
+    if (ece391_getargs(args, sizeof(args)) != 0) {
+        ece391_fdputs(1, (uint8_t *)"Could not read args\n");
+        return 1;
+    }
+
+    ece391_fdputs(1, args);
+    ece391_fdputs(1, "\n");
+    return 0;
+}
