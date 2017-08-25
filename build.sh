f7497c4365f51b4e7c52ab8da2d64240093265d9
Andrew Sun <0xandrewsun@gmail.com>
2017-08-25 11:42:10 -0500

    Update scripts

--- build.sh
+++ build.sh
@@ -9,7 +9,6 @@ if [ "$EUID" -eq 0 ]; then
 fi
 
 # Make binaries executable
-chmod +x "${mp3_dir}/createfs"
 chmod +x "${mp3_dir}/elfconvert"
 
 # Compile userspace programs
@@ -17,21 +16,12 @@ mkdir -p "${mp3_dir}/syscalls/to_fsdir"
 make -C "${mp3_dir}/syscalls"
 cp "${mp3_dir}/syscalls/to_fsdir/"* "${mp3_dir}/fsdir"
 
-# Compile fish program
-make -C "${mp3_dir}/fish"
-cp "${mp3_dir}/fish/fish" "${mp3_dir}/fsdir"
-cp "${mp3_dir}/fish/frame0.txt" "${mp3_dir}/fsdir"
-cp "${mp3_dir}/fish/frame1.txt" "${mp3_dir}/fsdir"
-
-# Copy fsdir to tmp and create the rtc file
-rm -rf "/tmp/fsdir"
-cp -r "${mp3_dir}/fsdir" "/tmp/fsdir"
-sudo mknod "/tmp/fsdir/rtc" c 10 61
-
 # Generate new filesystem image
 rm -f "${mp3_dir}/student-distrib/filesys_img"
 python "${mp3_dir}/createfs.py" -i "${mp3_dir}/fsdir" -o "${mp3_dir}/student-distrib/filesys_img"
 
 # Build OS image
+make -C "${mp3_dir}/student-distrib"
 cd "${mp3_dir}/student-distrib"
 sudo "./debug.sh"
+cd "${mp3_dir}"
