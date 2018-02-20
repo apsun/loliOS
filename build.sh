3838198e57952cd31cfcca86e0d98455d19c2730
Andrew Sun <0xandrewsun@gmail.com>
2018-02-20 00:51:54 -0600

    Rename directories

--- build.sh
+++ build.sh
@@ -10,8 +10,8 @@ fi
 
 # If first arg is "clean", run make clean
 if [ "$1" == "clean" ]; then
-    make -C "${mp3_dir}/syscalls" clean
-    make -C "${mp3_dir}/student-distrib" clean
+    make -C "${mp3_dir}/userspace" clean
+    make -C "${mp3_dir}/kernel" clean
     exit 0
 fi
 
@@ -19,16 +19,16 @@ fi
 chmod +x "${mp3_dir}/elfconvert"
 
 # Compile userspace programs
-mkdir -p "${mp3_dir}/syscalls/to_fsdir"
-make -C "${mp3_dir}/syscalls"
-cp "${mp3_dir}/syscalls/to_fsdir/"* "${mp3_dir}/fsdir"
+mkdir -p "${mp3_dir}/userspace/build"
+make -C "${mp3_dir}/userspace"
+cp "${mp3_dir}/userspace/build/"* "${mp3_dir}/filesystem"
 
 # Generate new filesystem image
-rm -f "${mp3_dir}/student-distrib/filesys_img"
-python "${mp3_dir}/createfs.py" -i "${mp3_dir}/fsdir" -o "${mp3_dir}/student-distrib/filesys_img"
+rm -f "${mp3_dir}/kernel/filesys_img"
+python "${mp3_dir}/createfs.py" -i "${mp3_dir}/filesystem" -o "${mp3_dir}/kernel/filesys_img"
 
 # Build OS image
-make -C "${mp3_dir}/student-distrib"
-cd "${mp3_dir}/student-distrib"
+make -C "${mp3_dir}/kernel"
+cd "${mp3_dir}/kernel"
 sudo "./debug.sh"
 cd "${mp3_dir}"
