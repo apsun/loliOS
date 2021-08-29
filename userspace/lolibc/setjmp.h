5497735f2e91f653939085cfa4e96e5791c9ad61
Andrew Sun <andrew@crossbowffs.com>
2021-08-29 16:51:12 -0700

    More libc namespace cleanup

--- userspace/lolibc/setjmp.h
+++ userspace/lolibc/setjmp.h
@@ -3,8 +3,6 @@
 
 #include <stdint.h>
 
-#define __cdecl __attribute__((cdecl))
-
 typedef struct {
     uint32_t eip;
     uint32_t esp;
@@ -14,8 +12,8 @@ typedef struct {
     uint32_t edi;
 } jmp_buf;
 
-__cdecl void longjmp(jmp_buf env, int status);
-__cdecl int __setjmp_ptr(jmp_buf *env);
+__attribute__((cdecl)) void longjmp(jmp_buf env, int status);
+__attribute__((cdecl)) int __setjmp_ptr(jmp_buf *env);
 #define setjmp(env) __setjmp_ptr(&(env))
 
 #endif /* _LOLIBC_SETJMP_H */
