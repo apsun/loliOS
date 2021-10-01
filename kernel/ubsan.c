8f6677e08ff75f09f901b1a703fd777ee5c7aae6
Andrew Sun <andrew@crossbowffs.com>
2021-09-30 23:21:40 -0700

    Add type params to ubsan handlers

--- kernel/ubsan.c
+++ kernel/ubsan.c
@@ -9,27 +9,28 @@ typedef struct {
     int col;
 } ubsan_source_loc_t;
 
-#define MAKE_UBSAN_HANDLER(name)                      \
-    __noreturn __used void                            \
-    __ubsan_handle_##name(ubsan_source_loc_t *source) \
-    {                                                 \
-        panic(                                        \
-            "UBSan detected %s at %s:%d:%d\n",        \
-            #name,                                    \
-            source->file,                             \
-            source->line,                             \
-            source->col);                             \
+#define MAKE_UBSAN_HANDLER(name, ...)              \
+    __noreturn __used void                         \
+    __ubsan_handle_##name(void *s, ## __VA_ARGS__) \
+    {                                              \
+        ubsan_source_loc_t *source = s;            \
+        panic(                                     \
+            "UBSan detected %s at %s:%d:%d\n",     \
+            #name,                                 \
+            source->file,                          \
+            source->line,                          \
+            source->col);                          \
     }
 
-MAKE_UBSAN_HANDLER(add_overflow)
+MAKE_UBSAN_HANDLER(add_overflow, void *lhs, void *rhs)
 MAKE_UBSAN_HANDLER(builtin_unreachable)
-MAKE_UBSAN_HANDLER(divrem_overflow)
-MAKE_UBSAN_HANDLER(mul_overflow)
-MAKE_UBSAN_HANDLER(negate_overflow)
-MAKE_UBSAN_HANDLER(out_of_bounds)
-MAKE_UBSAN_HANDLER(pointer_overflow)
-MAKE_UBSAN_HANDLER(shift_out_of_bounds)
-MAKE_UBSAN_HANDLER(sub_overflow)
-MAKE_UBSAN_HANDLER(type_mismatch_v1)
+MAKE_UBSAN_HANDLER(divrem_overflow, void *lhs, void *rhs)
+MAKE_UBSAN_HANDLER(mul_overflow, void *lhs, void *rhs)
+MAKE_UBSAN_HANDLER(negate_overflow, void *val)
+MAKE_UBSAN_HANDLER(out_of_bounds, void *index)
+MAKE_UBSAN_HANDLER(pointer_overflow, void *base, void *result)
+MAKE_UBSAN_HANDLER(shift_out_of_bounds, void *lhs, void *rhs)
+MAKE_UBSAN_HANDLER(sub_overflow, void *lhs, void *rhs)
+MAKE_UBSAN_HANDLER(type_mismatch_v1, void *ptr)
 
 #endif /* UBSAN_ENABLED */
